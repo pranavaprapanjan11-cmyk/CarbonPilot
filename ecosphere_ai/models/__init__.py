e75cf3308447071839df690e261fafc697b9148f
Pranava prapanjan S S <pranavaprapanjan11@gmail.com>
2026-07-12 17:33:19 +0530

    Add complete backend implementation

---
+++ ecosphere_ai/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import hr_extension
+from . import environmental
+from . import social
+from . import governance
+from . import gamification
+from . import scoring_engine
